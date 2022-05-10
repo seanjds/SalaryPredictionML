0a8fa0509b94e6d648774c9bd1ef69f42ebfd98e
seanjds <61580676+seanjds@users.noreply.github.com>
2022-05-10 19:22:42 -0400

    change

--- predict_page.py
+++ predict_page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
+import sklearn
 
 def load_model():
     with open('saved_steps.pkl', 'rb') as file:
